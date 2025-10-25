674be30318cb353b208be977316f0b89386eec5f
Sourav Kumar <souravkr2592004@gmail.com>
2025-10-25 20:44:25 +0530

    calculator

---
+++ calc.js
@@ -0,0 +1,31 @@
+let inputBox = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');  
+
+let string = "";
+
+buttons.forEach(element => {
+    element.addEventListener('click',(e)=>{
+        if(e.target.innerText == '='){
+            string = String(eval(string));
+            inputBox.value = string;
+        }
+        else if(e.target.innerText == 'AC'){
+            string = "";
+            inputBox.value = string;
+        }
+        else if(e.target.innerText == 'DEL'){
+            string = string.slice(0,- 1);
+            inputBox.value = string;
+        }
+        else if(e.target.id == 'plusMinus'){
+            string = String(-eval(string))
+            inputBox.value = string;
+        }
+          
+        else{
+            string += e.target.innerText;
+            inputBox.value = string;
+        }    
+    })
+
+})
